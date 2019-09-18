a422299434e5e7e0807ea9ecce87b90923680c35
yuriy-m-dev <fath66426@gmail.com>
2019-09-18 18:49:28 +0300

    added products and product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-    
+    var mixer = mixitup('.products__inner-box')
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
