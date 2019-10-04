81331019d7f74c2c62b54f9052c76a077a46e491
yuriy-m-dev <fath66426@gmail.com>
2019-10-04 17:21:31 +0400

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,32 @@ $(function(){
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1440,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 801,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+          ]
     });
 
     $('input[type="file"], select').styler();
